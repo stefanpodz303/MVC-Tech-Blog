c2b9b15f2f10191e149828d6e00241de30bc1f9d
The Podz <stefanpodz@yahoo.com>
2021-04-30 16:02:02 -0400

    added folders and files for base of app and updated models and schema

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // user must enter a username
+        notEmpty: true,
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        // user must enter a valid email
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      // user must enter a password
+      allowNull: false,
+      validate: {
+        // password must be at least 6 characters
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,41 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment')
+
+// User to Post relationship
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+// Post to User relationship
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+// Comment to User relationship
+Comment.belongsTo(User, {
+  foreignKey:'user_id',
+  onDelete: 'cascade',
+  hooks:true
+});
+
+// Comment to Post relationship
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks: true
+});
+
+// User to Comment relationsihp
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+// Post to Comment relationship
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks:true
+})
+
+module.exports = { User, Post, Comment };
